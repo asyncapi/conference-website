3ce75dcfa05b430a65421056345aadd47f89378a
AceTheCreator <devaze007@gmail.com>
2022-05-24 16:24:03 +0100

    add svg illustrations

--- components/Form/form.js
+++ components/Form/form.js
@@ -1,4 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Avatar from "../illustrations/avatar";
+import Invitation from "../illustrations/invite";
+import Calendar from "../illustrations/calendar";
+import Location from "../illustrations/location";
+
+const fields = [
+  {
+    title: "Your Name",
+    description: "Your Details",
+    icon: <Avatar />,
+  },
+  {
+    title: "Preferable Type",
+    description: "Choose the type peferable",
+    icon: <Invitation />,
+  },
+  {
+    title: "Location",
+    description: "Desired location",
+    icon: <Location />,
+  },
+  {
+    title: "Date",
+    description: "Best Date",
+    icon: <Calendar />,
+  },
+];
 
 function form() {
   return (
@@ -13,6 +40,18 @@ function form() {
           borderBottom: "2px solid #2b2146",
         }}
       />
+      <div className="flex">
+        <div
+          className="w-1/3"
+          style={{
+              borderRight: "2px solid #2b2146",
+              minHeight: "50vh"
+          }}
+        >
+          hello
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
