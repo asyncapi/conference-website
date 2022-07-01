a42eccdbdaaf3549d17d9e12c656032c31a3db87
Ace <40604284+AceTheCreator@users.noreply.github.com>
2022-07-01 12:01:17 +0100

    fix: updated website with event date and location (#99)

--- components/Header/header.js
+++ components/Header/header.js
@@ -2,18 +2,18 @@
 import React from "react";
 import Scrollspy from "react-scrollspy";
 import Pattern1 from "../illustrations/pattern1";
-import Polygon from "../illustrations/polygon"
+import Pattern2 from "../illustrations/pattern2";
 
 function Header() {
   return (
     <div
       style={{
         position: "relative",
-        height: "70vh",
+        height: "91vh",
       }}
     >
       <div className="absolute">
-        <img src="dots.svg" alt="dots"/>
+        <img src="dots.svg" alt="dots" />
       </div>
       <div className="absolute right-0">
         <Pattern1 className="w-72 sm:w-32" />
@@ -29,34 +29,43 @@ function Header() {
           <div className="text-center mt-16">
             <div className="flex items-center justify-center">
               <h3 className="text-fainted-paint text-xl sm:text-lg">
-                November, 2022
+                November 3-5, 2022
               </h3>
               <span className="ml-2 text-fainted-white">|</span>
               <h3 className="ml-2 text-fainted-paint font-bold text-xl  sm:text-lg">
-                In-person & Virtual
+                Barcelona, Spain
               </h3>
             </div>
+            <h3 className="mt-4 text-fainted-paint font-bold text-xl  sm:text-lg">
+              In-person & Virtual
+            </h3>
           </div>
           <div className="flex items-center justify-center mt-12">
             <div className="w-96 h-12 p-2 bg-dark-paint rounded-md flex items-center justify-between">
               <input
                 disabled={true}
-                placeholder="Click the button to start survey"
-                className="h-full bg-transparent outline-none text-white w-5/6"
+                placeholder="Call for Proposals"
+                className="h-full bg-transparent outline-none text-white w-11/12"
               />
-              <Scrollspy items={["forms"]}>
-                <a href="#forms">
-                  <button className="bg-tetiary-pink p-2 rounded-md text-white">
-                    Start
-                  </button>
-                </a>
-              </Scrollspy>
+              <a
+                href="https://linuxfoundation.smapply.io/prog/asyncapi_conference_2022/"
+                target="_blank"
+                rel="noreferrer"
+                className="w-6/12"
+              >
+                <button className="bg-tetiary-pink p-2 w-full rounded-md text-white">
+                  Apply now
+                </button>
+              </a>
             </div>
           </div>
         </div>
       </div>
-      <div className="absolute bottom-0 opacity-50 sm:hidden">
-       <Polygon className="w-72" />
+      <div className="absolute bottom-0 right-0 rotate-180 ">
+        <img src="dots.svg" alt="dots" />
+      </div>
+      <div className="absolute bottom-0 rotate-0 opacity-50">
+        <Pattern2 className="w-72 sm:w-32" />
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Header from '../components/Header/header';
 import { Toaster } from "react-hot-toast";
 import Navbar from "../components/Navbar/navbar";
-import Form from "../components/Form/form";
+// import Form from "../components/Form/form";
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
         <Toaster  />
         <Navbar />
         <Header />
-        <Form />
+        {/* <Form /> */}
       </main>
     </div>
   );
