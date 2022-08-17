d4b26f9167979f6bc0eee2f7b573ca00c9794217
acethecreator <devaze007@gmail.com>
2022-08-17 11:39:01 +0100

    added heading illustration

---
+++ components/illustrations/stepLike.js
@@ -0,0 +1,142 @@
+export default function Step() {
+  return (
+    <svg
+      width="338"
+      height="409"
+      viewBox="0 0 338 409"
+      fill="none"
+    >
+      <path
+        d="M184.457 1L123.289 36.8708L276.185 125.477L337.352 89.5832L184.457 1Z"
+        fill="#E8E2F4"
+        stroke="#2E214A"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M123.289 36.8707V248.677L276.185 337.26V125.477L123.289 36.8707Z"
+        fill="#E8E2F4"
+      />
+      <path
+        d="M123.289 36.8707V248.677L276.185 337.26V125.477L123.289 36.8707Z"
+        fill="#BDBCEE"
+      />
+      <path
+        d="M123.289 36.8707V248.677L276.185 337.26V125.477L123.289 36.8707Z"
+        stroke="#2E214A"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M123.313 106.63L62.168 142.524L215.064 231.107L276.208 195.236L123.313 106.63Z"
+        fill="#E8E2F4"
+        stroke="#2E214A"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M62.168 142.524V283.933L215.063 372.539V231.107L62.168 142.524Z"
+        fill="#E8E2F4"
+      />
+      <path
+        d="M62.168 142.524V283.933L215.063 372.539V231.107L62.168 142.524Z"
+        fill="#BDBCEE"
+      />
+      <path
+        d="M62.168 142.524V283.933L215.063 372.539V231.107L62.168 142.524Z"
+        stroke="#2E214A"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M62.1445 212.784L1 248.678L153.895 337.261L215.04 301.39L62.1445 212.784Z"
+        fill="#E8E2F4"
+        stroke="#2E214A"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1 248.678V319.394L153.895 408V337.261L1 248.678Z"
+        fill="#E8E2F4"
+      />
+      <path
+        d="M1 248.678V319.394L153.895 408V337.261L1 248.678Z"
+        fill="#BDBCEE"
+      />
+      <path
+        d="M1 248.678V319.394L153.895 408V337.261L1 248.678Z"
+        stroke="#2E214A"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M337.352 89.5835V301.39L276.185 337.261V125.477L337.352 89.5835Z"
+        fill="#E8E2F4"
+      />
+      <path
+        d="M337.352 89.5835V301.39L276.185 337.261V125.477L337.352 89.5835Z"
+        fill="url(#paint0_linear_35_1038)"
+      />
+      <path
+        d="M337.352 89.5835V301.39L276.185 337.261V125.477L337.352 89.5835Z"
+        stroke="#2E214A"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M276.208 195.236V336.645L215.063 372.539V231.107L276.208 195.236Z"
+        fill="#E8E2F4"
+      />
+      <path
+        d="M276.208 195.236V336.645L215.063 372.539V231.107L276.208 195.236Z"
+        fill="url(#paint1_linear_35_1038)"
+      />
+      <path
+        d="M276.208 195.236V336.645L215.063 372.539V231.107L276.208 195.236Z"
+        stroke="#2E214A"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M215.041 301.39V372.129L153.896 408V337.261L215.041 301.39Z"
+        fill="#E8E2F4"
+      />
+      <path
+        d="M215.041 301.39V372.129L153.896 408V337.261L215.041 301.39Z"
+        fill="url(#paint2_linear_35_1038)"
+      />
+      <path
+        d="M215.041 301.39V372.129L153.896 408V337.261L215.041 301.39Z"
+        stroke="#2E214A"
+        strokeLinejoin="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_35_1038"
+          x1="278.469"
+          y1="15.4674"
+          x2="411.678"
+          y2="47.3004"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#2DCCFD" />
+          <stop offset="1" stopColor="#AD20E2" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_35_1038"
+          x1="217.346"
+          y1="142.179"
+          x2="344.006"
+          y2="184.445"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#2DCCFD" />
+          <stop offset="1" stopColor="#AD20E2" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_35_1038"
+          x1="156.179"
+          y1="269.488"
+          x2="263.797"
+          y2="329.213"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#2DCCFD" />
+          <stop offset="1" stopColor="#AD20E2" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
