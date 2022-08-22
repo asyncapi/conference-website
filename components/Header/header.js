788c73546bc2cea7716533f6f65d9d2d92b45e30
acethecreator <devaze007@gmail.com>
2022-08-22 11:36:23 +0100

    ehanced header data

--- components/Header/header.js
+++ components/Header/header.js
@@ -7,13 +7,56 @@ import Stroke1 from "../illustrations/stroke1";
 import Stroke2 from "../illustrations/stroke2";
 import Step from "../illustrations/stepLike";
 
+const data = [
+  {
+    title: "What is the AsyncAPI Conference?",
+    description:
+      "At the 2022 AsyncAPI Conference, speakers from all around the world will take the stage in Barcelona, Spain where they will have the opportunity to present how they are using the AsyncAPI Specification in their real-world use cases, how they use tooling to support the specification, and how they have contributed to the project.",
+    button: (
+      <Button
+        text="Visit the AsyncAPI Website"
+        className="text-[21px] py-[14px] px-[26px] mt-[32px]"
+      />
+    ),
+  },
+  {
+    title: "Apply to be a speaker",
+    description:
+      "We are in the process of looking for those interested in giving a talk at the upcoming AsyncAPI Conference.",
+    button: (
+      <Button
+        text="Submit a proposal"
+        className="text-[21px] py-[14px] px-[26px] mt-4 btn-gradient"
+      />
+    ),
+  },
+  {
+    title: "Sign up for updates",
+    description:
+      "Subscribe to email notifications to receive updates on events like: when the official conference schedule is posted, virtual and in-person event details, and more.",
+    button: (
+      <div className="flex h-[56px] mt-[26px] items-center rounded-lg border border-dark-600">
+        <input
+          type="email"
+          className="bg-transparent py-[14px] px-[26px] h-full w-3/4 text-[18px] focus:outline-none indent-3"
+          placeholder="Your email address"
+        />
+        <Button
+          text="Subscribe"
+          className="text-sm h-full w-1/4 border-none rounded-none"
+        />
+      </div>
+    ),
+  },
+];
+
 function Header() {
   return (
     <div>
       <div className="mt-10 flex justify-center relative z-50 h-[43vh]">
         <div className="absolute flex flex-col items-center text-white w-2/4 sm:w-full">
           <Logo className="" height="112px" width="112px" />
-          <div className='text-center leading-[7rem]'>
+          <div className="text-center leading-[7rem]">
             <h1 className="mt-5 text-[100px] sm:text-4xl font-black">
               AsyncAPI
             </h1>
@@ -43,9 +86,9 @@ function Header() {
       <img
         src="/img/architecture.svg"
         alt="architecture"
-        className="mt-5 w-[100%]"
+        className="mt-[60px] w-[100%]"
       />
-      <div className="container mx-auto -mt-20 w-full flex justify-center items-center relative">
+      <div className="container mx-auto -mt-20 w-full flex justify-center items-center relative pb-[84px]">
         <div className="w-full relative">
           <div className="absolute bg-red w-full">
             <div className="right-[15rem] top-[120px] absolute">
@@ -57,53 +100,20 @@ function Header() {
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4 text-white">
-            <div className="h-fit bg-[rgba(36, 30, 49, 0.8)] backdrop-blur-sm border rounded-lg border-dark-600 p-14">
-              <h1 className="font-bold text-lg mb-4">
-                What is the AsyncAPI Conference?
-              </h1>
-              <p className="text-xs text-dark-500 leading-4">
-                At the 2022 AsyncAPI Conference, speakers from all around the
-                world will take the stage in Barcelona, Spain where they will
-                have the opportunity to present how they are using the AsyncAPI
-                Specification in their real-world use cases, how they use
-                tooling to support the specification, and how they have
-                contributed to the project.
-              </p>
-              <Button
-                text="Visit the AsyncAPI Website"
-                className="text-sm p-2 mt-4"
-              />
-            </div>
-            <div className="h-fit bg-[rgba(36, 30, 49, 0.8)] backdrop-blur-sm border rounded-lg border-dark-600 p-14">
-              <h1 className="font-bold text-lg mb-4">Apply to be a speaker</h1>
-              <p className="text-xs text-dark-500 leading-4">
-                We are in the process of looking for those interested in giving
-                a talk at the upcoming AsyncAPI Conference.
-              </p>
-              <Button
-                text="Submit a proposal"
-                className="text-sm p-2 mt-4 btn-gradient"
-              />
-            </div>
-            <div className="h-fit bg-[rgba(36, 30, 49, 0.8)] backdrop-blur-sm border rounded-lg border-dark-600 p-14">
-              <h1 className="font-bold text-lg mb-4">Sign up for updates</h1>
-              <p className="text-xs text-dark-500 leading-4">
-                Subscribe to email notifications to receive updates on events
-                like: when the official conference schedule is posted, virtual
-                and in-person event details, and more.
-              </p>
-              <div className="flex mt-4 items-center h-10 rounded-sm border border-dark-600">
-                <input
-                  type="email"
-                  className="bg-transparent h-full w-3/4 text-sm focus:outline-none indent-3"
-                  placeholder="Your email address"
-                />
-                <Button
-                  text="Subscribe"
-                  className="text-sm h-full w-1/4 border-none rounded-none"
-                />
+            {data.map((list, i) => (
+              <div
+                key={i}
+                className="h-fit bg-[rgba(36, 30, 49, 0.8)] backdrop-blur-sm border rounded-lg border-dark-600 p-14"
+              >
+                <h1 className="font-bold text-[32px] mb-4">
+                 {list.title}
+                </h1>
+                <p className="text-[18px] text-dark-500 leading-[29px]">
+                  {list.description}
+                </p>
+                {list.button}
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
