edeaf6d02cd02ed4b3cbcccbd9bb5b200acdc485
acethecreator <devaze007@gmail.com>
2022-09-22 12:42:33 +0100

    enhancing schedule page

--- components/Speakers/speakers.js
+++ components/Speakers/speakers.js
@@ -8,37 +8,39 @@ import Link from 'next/link';
 
 function Speakers() {
   return (
-    <div className="container mx-auto w-full speakers-bg h-[958px] relative overflow-hidden">
-      <div className="absolute right-0 top-[80px]">
-        <div className="relative h-[25rem]">
-          <Tower className="absolute right-[20px]" />
+    <div className="speakers-bg h-[958px] relative overflow-hidden">
+      <div className="container mx-auto w-full">
+        <div className="absolute right-0 top-[80px]">
+          <div className="relative h-[25rem]">
+            <Tower className="absolute right-[20px]" />
+          </div>
+          <Stack className="mt-[30px]" />
         </div>
-        <Stack className="mt-[30px]" />
-      </div>
-      <div className="pt-[100px]">
-        <h1 className="text-[84px] text-white tracking-tight">Speakers</h1>
-        <div className="mt-[46px]">
-          <CardSlider>
-            {speakers.map((speaker) => (
-              <div key={speaker.name} className="">
-                <Card
-                  title={speaker.name}
-                  alt={speaker.name}
-                  summary={`${speaker.title}, ${speaker.company}`}
-                  img={speaker.avatar}
-                />
-              </div>
-            ))}
-            <Card>
-              <div className="justify-center flex flex-col items-center h-full">
-                <Link href="/schedule">
-                  <h4 className="cursor-pointer underline">
-                    View all speakers & schedule
-                  </h4>
-                </Link>
-              </div>
-            </Card>
-          </CardSlider>
+        <div className="pt-[100px]">
+          <h1 className="text-[84px] text-white tracking-tight">Speakers</h1>
+          <div className="mt-[46px]">
+            <CardSlider>
+              {speakers.map((speaker) => (
+                <div key={speaker.name} className="">
+                  <Card
+                    title={speaker.name}
+                    alt={speaker.name}
+                    summary={`${speaker.title}, ${speaker.company}`}
+                    img={speaker.avatar}
+                  />
+                </div>
+              ))}
+              <Card>
+                <div className="justify-center flex flex-col items-center h-full">
+                  <Link href="/schedule">
+                    <h4 className="cursor-pointer underline">
+                      View all speakers & schedule
+                    </h4>
+                  </Link>
+                </div>
+              </Card>
+            </CardSlider>
+          </div>
         </div>
       </div>
     </div>
