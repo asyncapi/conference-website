5cc24cec6b638b06a4d6c4e8a129775829c8e539
acethecreator <devaze007@gmail.com>
2022-09-22 14:11:16 +0100

    added the schedule page enhancement

--- pages/schedule/index.js
+++ pages/schedule/index.js
@@ -7,16 +7,62 @@ function index() {
   return (
     <div>
       <Navbar />
-      <div className="container text-white">
-        <h1 className="mt-5 text-[100px] sm:text-4xl font-black">Schedule</h1>
-        <p className="mt-4 text-dark-400 text-[28px]">
-          This schedule is automatically displayed in Coordinated Universal Time
-          (UTC). To see the schedule in your preferred timezone, please select
-          it from the Timezone drop-down menu to the right, above "Filter by
-          Date."
-        </p>
+      <div className="h-[450px] flex flex-col justify-between relative overflow-hidden">
+        <div className="schedule-bg" />
+        <div className='relative'>
+          <div className="absolute w-full flex justify-center z-[9]">
+            <div className="container text-white text-center w-[1200px]">
+              <h1 className="mt-5 text-[100px] sm:text-4xl font-black">
+                Schedule
+              </h1>
+              <p className="mt-4 text-dark-400 text-[28px]">
+                This schedule is automatically displayed in Coordinated
+                Universal Time (UTC). To see the schedule in your preferred
+                timezone, please select it from the Timezone drop-down menu to
+                the right, above "Filter by Date."
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between overflow-hidden">
+          <img
+            src="/img/scheduleArcLeft.svg"
+            alt="architect"
+            className="w-[100%]"
+          />
+          <img
+            src="/img/scheduleArcRight.svg"
+            alt="architect"
+            className="w-[100%]"
+          />
+        </div>
+        {/* <div className="flex justify-between relative mt-60 overflow-hidden">
+          <img
+            src="/img/scheduleArcLeft.svg"
+            alt="architect"
+            className="w-[100%]"
+          />
+          <img
+            src="/img/scheduleArcRight.svg"
+            alt="architect"
+            className="w-[100%]"
+          />
+          <div className="absolute w-full -top-[230px] flex justify-center">
+            <div className="container text-white text-center w-[1200px]">
+              <h1 className="mt-5 text-[100px] sm:text-4xl font-black">
+                Schedule
+              </h1>
+              <p className="mt-4 text-dark-400 text-[28px]">
+                This schedule is automatically displayed in Coordinated
+                Universal Time (UTC). To see the schedule in your preferred
+                timezone, please select it from the Timezone drop-down menu to
+                the right, above "Filter by Date."
+              </p>
+            </div>
+          </div>
+        </div> */}
       </div>
-      <div className="mt-20 bg-white">
+      <div className="bg-white">
         <a id="sched-embed" href="//asyncapi2022.sched.com/"></a>
       </div>
       <Script
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
     },
     backgroundImage: {
       architectureCover: "url('/img/architecture.svg')",
+      schBg1: "url('/img/scheduleArcLeft.svg')",
     },
     extend: {
       colors: {
