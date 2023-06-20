075322707462f84a5a57c0fc5195986ee3bf04fd
acethecreator <devaze007@gmail.com>
2023-06-20 20:27:45 +0100

    footer content update

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -6,11 +6,7 @@ function Footer() {
           borderTop: "1px solid #333",
       }}>
           <div className='mt-2 text-lg text-gray-300 py-4'>
-<<<<<<< HEAD
             Copyright © AsyncAPI Project a Series of LF Projects, LLC
-=======
-              © 2023 AsyncAPI, Inc. All rights reserved
->>>>>>> 93965b1ecf4d23c9b0a89a531574eca9865bc52b
           </div>
       </div>
   )
