31c0669bad1c7b3af49ae02c2acaae1de04b570a
acethecreator <devaze007@gmail.com>
2023-07-21 14:51:03 +0100

    added the navbar

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,10 @@ import '../styles/globals.css';
 import { Toaster } from "react-hot-toast";
 function MyApp({ Component, pageProps }) {
   return (
-    <div className='container'>
       <div>
         <Toaster position="bottom-center" />
       <Component {...pageProps} />
       </div>
-    </div>
   );
 }
 
