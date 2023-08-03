5761ef209e47afb8a2a22eb5b61c5fe9521437c7
Lukasz Gornicki <lpgornicki@gmail.com>
2023-08-03 15:50:26 +0200

    Update subscription.js

--- components/Form/subscription.js
+++ components/Form/subscription.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Subscription() {
     return (
-        <form name="form 1" method="POST" className='flex sm:flex-col' data-netlify="true">
+        <form action='asyncapi-website.netlify.app' name="form 1" method="POST" className='flex sm:flex-col' data-netlify="true">
             <input type="hidden" name="form-name" value="form 1" />
             <input type='email' name='email' placeholder='Enter your email' required className='w-[90%] sm:w-full p-4 border border-black' />
             <button type='submit' className='p-4 bg-black text-white'>Subscribe</button>
