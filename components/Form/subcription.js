f93587fd285a99f765b0540857b005a309baf533
acethecreator <devaze007@gmail.com>
2023-08-03 14:01:44 +0100

    testing netlify form

--- components/Form/subcription.js
+++ components/Form/subcription.js
@@ -7,17 +7,19 @@ function Subcription() {
 				<h3 className='text-[32px] text-white lg:text-center'>
 					Subscribe for AACoT’23 updates!
 				</h3>
+
 				<form
-					name='form 1'
+					action='asyncapi-website.netlify.app'
 					method='POST'
-					data-netlify='true'
 					className='mt-[32px] flex items-center lg:flex-col lg:w-full'
 				>
 					<input type='hidden' name='form-name' value='form 1' />
+					<input type='text' name='name' placeholder='Name' />
 					<input
 						className='w-[728px] lg:w-full h-[84px] py-[8px] px-[16px] rounded-l-lg lg:rounded'
 						placeholder='Email Address'
 						type='email'
+						name='email'
 					/>
 
 					<button
