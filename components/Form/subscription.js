b434a084fe289244658c1b0782a39c9662a837e4
Ace <40604284+AceTheCreator@users.noreply.github.com>
2023-08-03 08:46:57 -0500

    fix: updated form with name (#158)

--- components/Form/subscription.js
+++ components/Form/subscription.js
@@ -4,7 +4,7 @@ function Subscription() {
     return (
         <form name="form 1" method="POST" className='flex sm:flex-col' data-netlify="true">
             <input type="hidden" name="form-name" value="form 1" />
-            <input type='email' placeholder='Enter your email' required className='w-[90%] sm:w-full p-4 border border-black' />
+            <input type='email' name='email' placeholder='Enter your email' required className='w-[90%] sm:w-full p-4 border border-black' />
             <button type='submit' className='p-4 bg-black text-white'>Subscribe</button>
       </form>
   )
