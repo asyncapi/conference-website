0734b99b51b5ed2a9aed5f5d48ba71f949b05c11
ankur0904 <ankursingh91002@gmail.com>
2023-09-22 21:19:55 +0530

    Adds target tag

--- pages/index.js
+++ pages/index.js
@@ -152,7 +152,7 @@ export default function Home() {
 											us on stage and share your valuable insights with our
 											enthusiastic audience!
 										</Paragraph>
-										<a href='https://apidays.typeform.com/to/ILJeAaV8#event_name=xxxxx'>
+										<a href='https://apidays.typeform.com/to/ILJeAaV8#event_name=xxxxx' target="_blank">
 											<Button className='mt-[80px] w-[244px] border border-gray card-bg'>
 												Apply as a Speaker
 											</Button>
