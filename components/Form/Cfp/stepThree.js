72e14fde1d08f36346acba47edc93dffd2153c96
acethecreator <devaze007@gmail.com>
2023-09-25 18:49:31 +0100

    added cfp form for bangalore

---
+++ components/Form/Cfp/stepThree.js
@@ -0,0 +1,92 @@
+/* eslint-disable react/no-unescaped-entities */
+import React, { useState, useEffect } from "react";
+import Select from "../select";
+import Button from "../../Buttons/button";
+
+const options1 = [
+  {
+    value: "Lightning talk",
+    label: "Lightning talk(5mins)",
+  },
+  {
+    value: "Session",
+    label: "Session(45mins)",
+  },
+    {
+    value: "Tutorial",
+    label: "Tutorial(1h)",
+  },
+  {
+    value: "Keynote",
+    label: "Keynote(45mins)",
+  },
+];
+
+const options2= [
+  {
+    value: "Introductory and overview",
+    label: "Introductory and overview",
+  },
+  {
+    value: "Intermediate",
+    label: "Intermediate",
+  },
+    {
+    value: "Advance",
+    label: "Advance",
+  },
+  {
+    value: "Expert",
+    label: "Expert",
+  },
+];
+
+function StepThree({ setStep, setForm, data }) {
+  const [value, setValue] = useState({});
+  useEffect(() => {
+    setForm({ ...data, ...value});
+  }, [value]); 
+  return (
+    <form className="mt-3 w-[30rem] lg:w-[auto]" onSubmit={(e) => setStep(e, 4)}>
+      <h1 className="text-white font-bold text-4xl lg:text-3xl">
+        Session Info
+      </h1>
+      <p className="mt-3 text-dark-600">
+       Please provide your session title and description
+      </p>
+      <div className="mt-3 border w-full border-solid border-y-dark-600 divide-y" />
+          <div className="mt-10">
+        <div className="text-dark-600 text-lg mb-4">Session Format</div>
+        <Select
+          options={options1}
+          title="Select session format"
+          setValue={(val) => setValue({...value, Format: val})}
+          multi={false}
+        />
+        <div className="text-dark-600 text-lg mt-4 mb-4">Session Level</div>
+        <Select
+          options={options2}
+          title="Select session level"
+          setValue={(val) => setValue({...value, Level: val})}
+          multi={false}
+        />
+        <div className="float-right">
+        <a
+            className="mr-10 text-dark-600 cursor-pointer"
+            onClick={() => setStep(null, 1)}
+          >
+            Back
+                  </a>
+                  <Button
+                    type="submit"
+                    disabled={!data.Format || !data.Level && true}
+                    className="p-3 rounded-md mt-3 w-36">
+                      Next
+                  </Button>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default StepThree;
---
+++ pages/cfp/index.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect, useRef } from 'react'
+import Confetti from "react-confetti";
+import StepOne from "../../components/Form/Cfp/stepOne";
+import StepTwo from "../../components/Form/Cfp/stepTwo";
+import StepThree from '../../components/Form/Cfp/stepThree';
+import StepFour from '../../components/Form/Cfp/stepFour';
+
+const fields = [
+  {
+    title: "Your Information",
+    description: "Your details",
+    icon: "",
+  },
+  {
+    title: "Session Information",
+    description: "Session title and description",
+    icon: "",
+  },
+  {
+    title: "Session Format",
+    description: "Session format and level",
+    icon: "",
+  },
+  {
+    title: "Additional Information",
+    description: "Special request to organizers",
+    icon: "",
+  },
+];
+
+
+
+function Paper() {
+  const [step, setStep] = useState(1);
+  const confetiRef = useRef(null);
+  const [height, setHeight] = useState(null);
+  const [width, setWidth] = useState(null);
+  const [formData, setFormData] = useState({});
+  const onStepUpdate = (e, step) => {
+    if (e) {
+      e.preventDefault();
+    }
+    setStep(step);
+  };
+  useEffect(() => {
+    setHeight(confetiRef.current.clientHeight);
+    setWidth(confetiRef.current.clientWidth);
+  }, []);
+  const stepOne =  <StepOne setStep={onStepUpdate} setForm={setFormData} data={formData} />
+  let view = stepOne
+  if (step === 4) {
+    view = stepOne
+  }
+  if (step === 2) {
+    view = (
+      <StepTwo setStep={onStepUpdate} setForm={setFormData} data={formData} />
+    );
+  }
+  if (step === 3) {
+    view = (
+      <StepThree setStep={onStepUpdate} setForm={setFormData} data={formData} />
+    );
+  }
+  if (step === 4) {
+    view = (
+      <StepFour setStep={onStepUpdate} setForm={setFormData} data={formData} />
+    );
+  }
+  if (step === "successful") {
+    setTimeout(() => {
+      setStep(null, 1)
+    }, 6000)
+    view = <div  className='flex items-center h-full'>
+      <div>
+        <h1 className='text-fainted-white text-4xl font-bold'>Hurray!!!</h1>
+          <h1 className='text-fainted-white text-4xl font-bold mt-6'>Your talk has been submitted successfully</h1>
+      </div>
+       <Confetti numberOfPieces={50} width={width} height={height} tweenDuration={40} />
+    </div>
+  }
+
+    return (
+        <div className='container'>
+            <div className="relative mt-10 sm:mt-0" id="forms" ref={confetiRef}>
+                <h1 className="text-white font-bold text-5xl lg:text-3xl">
+        Submit your talk!
+      </h1>
+      <p className="mt-2 text-dark-500 text-lg">
+        Fill up the form to apply as a speaker for AACoT London Edition.
+      </p>
+      <div
+        className="mt-5"
+        style={{
+          borderBottom: "1px solid #333",
+        }}
+      />
+      <div className="flex lg:flex-col">
+        <div
+          className="lg:hidden"
+          style={{
+            borderRight: "1px solid #333",
+            minHeight: "50vh",
+          }}
+        >
+          <div className="p-6 lg:p-0 lg:py-2 lg:pr-4 mt-12">
+            {fields.map((field, i) => {
+              const index = i ;
+              return (
+                <div
+                      key={field.title}
+                      className='h-[100px]'
+                >
+                  <div className="flex justify-between">
+                    <div className="md:hidden">
+                      <h3 className="text-white font-bold text-lg">
+                        {field.title}
+                      </h3>
+                      <p className="text-dark-600">{field.description}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="p-10 lg:p-1">
+          <p className="text-fainted-white">{typeof(step) === 'number' && `Step ${step}/4`}</p>
+          {view}
+          <div
+            className="absolute bottom-0 right-0 rotate-0 opacity-50 sm:hidden"
+            style={{
+              transform: "scaleX(-1)",
+            }}
+          >
+          </div>
+        </div>
+      </div>
+      </div>
+      </div>
+  )
+}
+
+export default Paper
