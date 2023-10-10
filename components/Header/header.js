400e1eafee419cbabffeab367fb8e37538a8def7
thulieblack <sibanda.thulie@gmail.com>
2023-10-10 16:40:26 +0200

    update-madrid-pagr

--- components/Header/header.js
+++ components/Header/header.js
@@ -97,11 +97,11 @@ function Header() {
 							</div>
 							<div className='mt-[54px] relative flex items-center justify-center'>
 								<a
-									href='https://forms.gle/hJU1KE7gX2R5j7xc9'
+									href='https://www.eventbrite.com/e/entradas-asyncapi-conf-on-tour-2023-735378636297?aff=ebdssbdestsearch&keep_tld=1'
 									target='_blank'
 									rel='noreferrer'
 								>
-									<Button className='w-[200px]'>Get Free Tickets</Button>
+									<Button className='w-[200px]'>Register for Madrid</Button>
 								</a>
 							</div>
 						</div>
