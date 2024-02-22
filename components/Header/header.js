3ed84e63514619134e044a3d7ea7ef1a91bad1dd
V Thulisile Sibanda <66913810+thulieblack@users.noreply.github.com>
2024-02-22 13:35:44 +0200

    Update components/Header/header.js

--- components/Header/header.js
+++ components/Header/header.js
@@ -50,7 +50,7 @@ function Header() {
 					className='absolute sm:hidden flex items-center justify-center'
 					style={{
 						display: isTablet && 'none',
-						top: '200px',
+						top: '250px',
 					}}
 				>
 					<div className='globe-overlay'></div>
