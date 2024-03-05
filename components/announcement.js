dba7b81a6c725c07ffc9b6227c5258eed9808858
acethecreator <devaze007@gmail.com>
2024-03-05 08:54:18 +0100

    changed button placeholder

--- components/announcement.js
+++ components/announcement.js
@@ -3,9 +3,9 @@ import Link from 'next/link';
 
 export default function Announcement() {
   return (
-    <Link href="https://www.asyncapi.com/docs/reference/specification/v3.0.0" passHref={true}>
+    <Link href="https://apidays.typeform.com/to/ILJeAaV8?typeform-source=www.apidays.global#event_name=xxxxx" passHref={true}>
     <a rel="noopener noreferrer" target="_blank" >
-    <div className='border text-white w-[300px] rounded-lg p-1 text-center text-lg'>Call for speaker Helsinki is open!</div>
+    <div className='border text-white w-[300px] rounded-lg p-1 text-center text-lg'>Call for Speakers Helsinki is now open!</div>
     </a>
     </Link>
   )
