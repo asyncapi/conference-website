05fd4e2995a2e0cb88dc6dccd731684107cc1862
acethecreator <devaze007@gmail.com>
2024-03-05 09:37:46 +0100

    added links length

--- components/announcement.js
+++ components/announcement.js
@@ -5,7 +5,7 @@ export default function Announcement() {
   return (
     <Link href="https://apidays.typeform.com/to/ILJeAaV8?typeform-source=www.apidays.global#event_name=xxxxx" passHref={true}>
     <a rel="noopener noreferrer" target="_blank" >
-    <div className='border text-white w-[300px] rounded-lg p-1 text-center text-lg'>Call for Speakers Helsinki is now open!</div>
+    <div className='border text-white min-w-[300px] rounded-lg p-1 text-center text-lg'>Call for Speakers Helsinki is now open!</div>
     </a>
     </Link>
   )
