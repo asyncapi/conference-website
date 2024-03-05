412ab3dae52e6b9d78fe01c3ec92f486bd80b858
acethecreator <devaze007@gmail.com>
2024-03-05 09:46:51 +0100

    .

--- components/announcement.js
+++ components/announcement.js
@@ -3,10 +3,8 @@ import Link from 'next/link';
 
 export default function Announcement() {
   return (
-    <Link href="https://apidays.typeform.com/to/ILJeAaV8?typeform-source=www.apidays.global#event_name=xxxxx" passHref={true}>
-    <a rel="noopener noreferrer" target="_blank" >
-    <div className='border text-white min-w-[300px] rounded-lg p-1 text-center text-lg'>Call for Speakers Helsinki is now open!</div>
-    </a>
+    <Link href="/venue/Helsinki">
+    <div className='cursor-pointer border text-white min-w-[300px] rounded-lg p-1 text-center text-lg'>Call for Speakers Helsinki is now open!</div>
     </Link>
   )
 }
