744655172d33801d506547be1acd76b4c90118ce
Ram Goel <rgoel766@gmail.com>
2024-03-11 22:02:57 +0530

    chore: keep only year in cards

--- components/PastEditionCard/index.js
+++ components/PastEditionCard/index.js
@@ -5,10 +5,7 @@ const PastEditonCard = ({ url }) => {
     return (
         <div className='p-4 w-1/3 sm:w-11/12 border-2 border-b-[5px] border-b-white border-violet-800 rounded-xl bg-violet-900'>
             <div className='flex items-center flex-wrap justify-between'>
-                <div>
-                    <p className='font-semibold tracking-widest opacity-50 text-xl text-white'>{year}</p>
-                    <h1 className='text-2xl text-white font-semibold'>AACoT&apos;{String(year).substring(2)}</h1>
-                </div>
+                <h1 className='text-2xl text-white font-semibold'>{year}</h1>
                 <div>
                     <a href={url} rel='noreferrer' target='_blank' className='flex items-center justify-center'>
                        <p className='mr-2 text-white'>View Website</p> 
