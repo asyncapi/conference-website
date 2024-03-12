8998bde82f6f808995dd8f1213b055434ae8bb68
Mintu <mintugogoi567@gmail.com>
2024-03-12 19:00:09 +0530

    make footer responsive and make some more changes

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -20,44 +20,36 @@ function Footer() {
   ];
   return (
     <div className="container">
-          <div className="w-full flex justify-between items-center p-4">
-            <img src="/img/logo.png" className="w-[150px]" />
-      <div className="mt-2 text-[14px] text-gray underline">
-        <a
-        href="https://github.com/asyncapi/.github/blob/master/CODE_OF_CONDUCT.md"
-        target="_blank"
-        rel="noreferrer"
-        className="hover:text-sky-500 duration-200 ease-in-out"
-        >
-              Code of Conduct
-            </a>
-          </div>
-      <div>
-      </div>
-        <div className="flex items-center justify-between sm:flex-col sm:items-start">
-          <div className="flex flex-col justify-between items-start gap-2 w-[284px] sm:mt-6">
-            <div className="flex justify-between items-center gap-3">
-              {socials.map((social, index) => {
-                return (
-                  <a
-                    key={index}
-                    href={social.href}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="w-12 h-12 rounded-lg border-[1.5px] border-[#556061] flex items-center justify-center hover:border-[#AD20E2] duration-150 ease-in-out"
-                  >
-                    <img
-                      src={social.imgUrl}
-                      alt={social.name}
-                      className="w-8 h-8"
-                    />
-                  </a>
-                );
-              })}
-            </div>
-          </div>
+      <div className="w-full flex justify-between items-center p-4">
+        <div>
+          <img src="/img/logo.png" className="w-[150px]" />
         </div>
-    </div>
+        <div className="mt-2 text-[14px] text-gray underline text-slate-600">
+          <a
+            href="https://github.com/asyncapi/.github/blob/master/CODE_OF_CONDUCT.md"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-sky-500 duration-200 ease-in-out"
+          >
+            Code of Conduct
+          </a>
+        </div>
+        <div className="flex items-center">
+          {socials.map((social, index) => {
+            return (
+              <a
+                key={index}
+                href={social.href}
+                target="_blank"
+                rel="noreferrer"
+                className="w-12 h-12 rounded-lg border-[1.5px] border-[#556061] flex items-center justify-center hover:border-[#AD20E2] duration-150 ease-in-out mx-2"
+              >
+                <img src={social.imgUrl} alt={social.name} className="w-8 h-8" />
+              </a>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
