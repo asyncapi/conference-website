de8be84c835d2d6659ad16c1bdbcc460797670e4
Monu <ms88006218@gmail.com>
2024-03-23 23:54:16 +0530

    feat: Handle past events

--- components/Venue/venue.js
+++ components/Venue/venue.js
@@ -1,26 +1,38 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 
+ function isEventEnded(date){
+	const eventDate = date.split(" ");
+	const month = eventDate[0];
+	const year = eventDate[eventDate.length-1];
+	const startDay = eventDate[1].split("-")[0];
+	return new Date(`${startDay} ${month} ${year}`) < Date.now()
+}
+
+
 function Venue({ className, city }) {
+
+	const eventEnded = isEventEnded(city.date);
+	const textColor = eventEnded ? "text-gray-300": "text-white";
+
 	return (
 		<div>
 			<div style={{'--image-url': `url(${city.img})`}} 
 			className='relative w-[300px] h-[400px] sm:w-[250px] sm:h-[350px] card-bg bg-[image:var(--image-url)] flex items-center justify-center p-4 cursor-pointer'>
 				<div className='flex justify-between flex-col w-full h-full'>
 					<div className='flex justify-between items-center'>
-						<div><div className={`border text-white text-md rounded-lg p-1 text-center mt-2 ${city.cfp ? 'block': 'hidden' }`}>cfp is open</div></div>
+						<div><div className={`border ${textColor} text-md rounded-lg p-1 text-center mt-2 ${city.cfp ? 'block': 'hidden' }`}>cfp is open</div></div>
 						<a href={city.map} target='_blank' className='w-8 h-8 bg-white rounded-xl flex items-center justify-center' rel="noreferrer">
 						<img src='/img/mapIcon.svg' className='w-6' />
 						</a>
 					</div>
 					<div></div>
-					<div className='text-white'>
+					<div className={textColor}>
 						<div>
 							<span className='text-lg font-bold'>{city.country}, {city.name}</span>
 						</div>
-						<div className='flex items-center justify-between w-full'>
-						<div className='border border-gray-400 rounded-lg p-1 text-center mt-2'>{city.date}</div>
-						</div>
+						<div className='inline-block border border-gray-400 rounded-lg py-1 px-2 text-center mt-2'>{city.date}</div>
+						<span className='text-xs text-white'> - {eventEnded ? "ENDED" : "UPCOMING"}</span>
 					</div>
 					</div>
 				</div>
@@ -29,3 +41,4 @@ function Venue({ className, city }) {
 }
 
 export default Venue;
+export {isEventEnded}
