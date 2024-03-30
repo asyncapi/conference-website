5661a7ae2c1f58babddf94039da431fceca591fc
TenzDelek <tibetdelek@gmail.com>
2024-03-31 00:09:43 +0530

    fix additional changes

--- components/Venue/venue.js
+++ components/Venue/venue.js
@@ -6,7 +6,7 @@ function Venue({ className, city }) {
 		<div>
 			<div style={{'--image-url': `url(${city.img})`}} 
 			className='relative w-[300px] h-[400px] sm:w-[250px] sm:h-[350px] card-bg bg-[image:var(--image-url)] flex items-center justify-center p-4 cursor-pointer'>
-				 <div className="absolute  inset-0 bg-black opacity-30 rounded-4xl"/>
+				 <div className="absolute  inset-0 bg-black opacity-30 rounded-3xl"/>
 				<div className='flex justify-between flex-col w-full h-full z-10'>
 					<div className='flex justify-between items-center'>
 						<div><div className={` text-md text-white text-md rounded-xl px-2 py-1 bg-[#3e4757]  text-sm text-center mt-2 ${city.cfp ? 'block': 'hidden' }`}>cfp is open</div></div>
@@ -20,7 +20,7 @@ function Venue({ className, city }) {
 							<span className='text-lg font-bold'>{city.country}, {city.name}</span>
 						</div>
 						<div className='flex items-center justify-between w-full'>
-						<div className='border text-sm border-[#1a2694] rounded-xl px-2 py-1 text-[#1a2694] bg-[#edeefd] text-center mt-2'>{city.date}</div>
+						<div className='border text-xs border-[#1a2694] rounded-2xl px-2 py-2 text-[#1a2694] bg-[#edeefd] text-center mt-2'>{city.date}</div>
 						</div>
 					</div>
 					</div>
