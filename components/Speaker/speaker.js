c09f88ad3b89b2bdc62d623671b390c33b48e2fd
acethecreator <devaze007@gmail.com>
2024-04-25 00:05:17 +0100

    added speakers list

--- components/Speaker/speaker.js
+++ components/Speaker/speaker.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Speaker({details, location, className}) {
   return (
 		<div
-			className={`w-auto text-center flex flex-col items-center card-bg h-auto rounded-md p-[27px] ${className}`}
+			className={`w-auto text-center flex flex-col items-center card h-auto rounded-md p-[27px] ${className}`}
 		>
 			<div className='w-[300px] h-[300px] lg:w-[250px] lg:h-[250px]'>
 				<img
@@ -21,10 +21,10 @@ function Speaker({details, location, className}) {
 				>
 					<div>
 						{' '}
-						<p className='mt-[6.6px] text-[20px] text-gray'>{details.title}</p>
+						<p className='mt-[6.6px] text-[18px] text-gray-500'>{details.title}</p>
 					</div>
 					<div>
-						<p className='mt-[6.6px] text-[20px] text-gray'>
+						<p className='mt-[6.6px] text-[20px] text-gradient'>
 							{location?.location}
 						</p>
 					</div>
