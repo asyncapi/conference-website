30924edf819aa0a522ba3e47d7f32e8fcd213285
V Thulisile Sibanda <66913810+thulieblack@users.noreply.github.com>
2024-07-29 10:14:23 +0200

    update cfp page

--- pages/venue/online/index.js
+++ pages/venue/online/index.js
@@ -10,15 +10,14 @@ export default function CallForSpeakers() {
       <div className="px-20 py-12">
             <div>
 
-            <h2 className="text-2xl font-bold mb-8 ">Hello! Thank you for your interest in speaking at AACOoT&apos;24 in Online</h2>
+            <h2 className="text-2xl font-bold mb-8 ">Hello! Thank you for your interest in speaking at AACoT'24 Online Edition</h2>
             <div className="text-lg mb-4 lg:text-base">
-            Get ready to be part of the ultimate celebration of cutting-edge technology and innovation with AsyncAPI Conf on Tour (AACoT)! 
-            Embark on a thrilling journey across the globe as we host a series of micro-conferences that will ignite your curiosity and fuel your passion. Join the brightest minds, passionate enthusiasts, and a vibrant community of experts and developers who are eager to share knowledge, explore the latest developments, and push the boundaries of what&#39;s possible. With insightful talks and interactive workshops, AACoT is your gateway to elevate your skills and unlock your full potential in the world of technology. Don&#39;t miss this one-of-a-kind event where you can learn, collaborate, and embrace the endless possibilities.
+             The AsyncAPI Conf on Tour is an official event created by the AsyncAPI Initiative. This conference is aimed primarily at the community to share and exchange experiences between existing users and new members. We aim to integrate new members into the community and expand their knowledge about the project.
             </div>
 
-            <div className="text-lg mb-14 lg:mb-10 lg:text-base">
+  {/*<div className="text-lg mb-14 lg:mb-10 lg:text-base">
             AACoT Madrid Edition will be hosted by <a href="https://www.ibm.com/" className="font-semibold text-blue-400">IBM</a>, one of the leading digital transformation companies in Spain. The conference will take place at one of IBM&#39;s offices in London, providing attendees with an opportunity to network with industry leaders and peers while exploring the city&#39;s vibrant culture. During the conference, attendees can expect to hear from industry experts including Fran Mendez, who will give a keynote on the history of AsyncAPI.
-            </div>
+            </div>*/}
 
             </div>
 
@@ -35,7 +34,7 @@ export default function CallForSpeakers() {
             <div className="mb-4">
             <div className="text-lg">Who can submit a talk</div>
             <ul className="list-disc flex gap-0 flex-col px-10">
-                <li className="text-sm pr-14 lg:pr-0">We&#39;re excited to have speakers from different parts of the globe sharing how they apply the AsyncAPI Specification in their practical use cases.</li>
+                <li className="text-sm pr-14 lg:pr-0">We're excited to have speakers from different parts of the globe sharing how they apply the AsyncAPI Specification in their practical use cases.</li>
                 <li className="text-sm pr-14 lg:pr-0">We encourage proposals from all individuals, regardless of their expertise level, to promote a diverse content track that welcomes everyone.</li>
             </ul>
             </div>
@@ -43,9 +42,8 @@ export default function CallForSpeakers() {
             <div className="mb-4">
             <div className="text-lg">Talk Style</div>
             <ul className="list-disc flex gap-0 flex-col px-10">
-                <li className="text-sm">Lighting talks/ Informative sessions (typically 30 mins incl Q&A)</li>
-                <li className="text-sm">Session Presentation (typically 45 mins incl Q&A)</li>
-                <li className="text-sm">Tutorial/Hands-on (typically 1 hour incl Q&A)</li>
+                <li className="text-sm">Informative/Session Presentation (typically 30 mins incl Q&A)</li>
+                <li className="text-sm">Tutorial/Hands-on (typically 45 mins incl Q&A)</li>
 
             </ul>
             </div>
@@ -53,7 +51,7 @@ export default function CallForSpeakers() {
             <div className="mb-4">
             <div className="text-lg">Talks we don&#39;t recommend</div>
             <ul className="list-disc flex gap-0 flex-col px-10">
-                <li className="text-sm">Marketing/Sales - rather share your experience or use case using our tools or specification.</li>
+                <li className="text-sm">Marketing/Sales - rather share your experience or use case using our tools or specifications.</li>
                 <li className="text-sm">Workshops</li>
             </ul>
             </div>
@@ -61,7 +59,7 @@ export default function CallForSpeakers() {
             <div className=" mb-4">
             <div className="text-lg">Talk Reviews</div>
             <ul className="list-disc flex gap-0 flex-col px-10">
-                <li className="text-sm pr-14 lg:pr-0">Reviews will be kept anonymous and reviewers will not have access to speaker information such as name, gender or company, etc.</li>
+                <li className="text-sm pr-14 lg:pr-0">Reviews will be kept anonymous, and reviewers will not have access to speaker information such as name, gender or company, etc.</li>
             </ul>
             </div>
 
@@ -78,12 +76,12 @@ export default function CallForSpeakers() {
 
         </div>
 
-        <h2 className="text-xl font-bold lg:text-base">P.S. This is an in-person event and we are not accepting virtual presentations.</h2>
+{/*<h2 className="text-xl font-bold lg:text-base">P.S. This is an in-person event and we are not accepting virtual presentations.</h2>*/}
 
       </div> {/* content div end */}
         <div className="flex justify-center my-14 lg:my-2 mb-20 lg:mb-14">
           <Link href="/venue/online/register">
-      <Button className="font-semibold w-96 lg:w-44">Submit Request</Button>
+      <Button className="font-semibold w-96 lg:w-44">Submit Proposal</Button>
           </Link>
         </div>
       </div>
