e7ed087ddf39626fa08a9bee39e281efc1a5cf62
Ashmit Personal <ashmitjagtap2209@gmail.com>
2024-08-11 15:17:43 +0530

    merge head

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -72,7 +72,7 @@ function Navbar() {
 		<div className='flex justify-center items-center sticky top-0 z-[99] text-white'>
 			<div className='w-[1131px]'>
 				<div className='p-5 flex justify-between h-[75px] w-full items-center'>
-					<div className='flex items-center sm:justify-between sm:w-full' data-test="nav-Home">
+					<div className='flex items-center sm:justify-between sm:w-full'>
 						<Link href='/'>
 							<div className='flex items-center cursor-pointer'>
 								<Image src='/img/logo.png' alt='conference logo' width={120} height={33} />
@@ -113,28 +113,18 @@ function Navbar() {
 													)}
 												</div>
 											) : (
-												<div data-test={`nav-${link.title}`}>
-												<Link href={link.ref} >{link.title}</Link>
-												</div>
+												<Link href={link.ref}>{link.title}</Link>
 											)}
 										</div>
-										{show === link.title && link.subMenu && (
-											<div data-test={`nav-${link.title}`}
-												className='subMenu absolute z-[9] mt-8 w-[140px] rounded-md left-[-15px] gradient-bg px-2 py-1 flex flex-col justify-center space-y-0'
-												onMouseEnter={handleSubMenuEnter}
-												onMouseLeave={handleSubMenuLeave}>
-												{link.subMenu.map((subL,i) => (
-													
+										{show && show === link.title && link.subMenu && (
+											<div
+												onMouseLeave={handleSubMenuLeave}
+												className='subMenu absolute z-[9] mt-8 w-[140px] rounded-md left-[-15px] gradient-bg pl-2 pt-1 flex flex-col justify-center space-y-0'>
+												{link.subMenu.map((subL) => (
 													<Link href={subL.ref} key={subL.title}>
-														{link.subMenu.length === 1 ? (
-															<div className='h-[32px] flex items-center justify-center text-[16px] hover:scale-95 hover:translate-x-1 transition-all'>
-																{subL.title}
-															</div>
-														) : (
-															<div className='h-[32px] text-[16px] hover:scale-95 hover:translate-x-1 transition-all'>
-																{subL.title}
-															</div>
-														)}
+														<div className='h-[32px] text-[16px] hover:scale-95 hover:translate-x-1 transition-all'>
+															{subL.title}
+														</div>
 													</Link>
 													
 												))}
