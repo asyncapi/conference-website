0d9d815e6cf99c2c0daf54edb5ebdeb67fedb7d1
Taufiq <taufiq9.2004@gmail.com>
2024-10-30 18:37:52 +0530

    added rel attribute

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -126,7 +126,7 @@ function Navbar() {
 												onMouseLeave={handleSubMenuLeave}
 											>
 												{link.subMenu.map((subL) => (
-													<Link href={subL.ref} target={subL.target} key={subL.title}>
+													<Link href={subL.ref} target={subL.target} key={subL.title} rel="noopener noreferrer">
 														<div className={`flex items-center ${link.subMenu.length === 1 ? "justify-center" : "justify-start"} min-h-[32px] text-[16px] hover:scale-95 hover:translate-x-1 transition-all`}>
 															{subL.title}
 														</div>
