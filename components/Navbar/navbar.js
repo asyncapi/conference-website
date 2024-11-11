7df3bc4959cb7969f39ac80c782968c4491dded9
r3yc0n1c <raja.majumdar.01@gmail.com>
2024-11-12 02:46:57 +0530

    feat: sticky hamburger menu for mobile version

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -69,7 +69,7 @@ function Navbar() {
 	};
 
 	return (
-		<div className='flex justify-center items-center sticky top-0 z-[99] text-white'>
+		<div className='flex justify-center items-center fixed w-full top-0 z-[99] text-white'>
 			<div className='w-[1131px]'>
 				<div className='p-5 flex justify-between h-[75px] w-full items-center'>
 					<div className='flex items-center sm:justify-between sm:w-full'>
