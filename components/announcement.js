c5a11d5c4639eb7cddaa7f70a80e29ee18c1641e
Rohan <r0hansingh@Rohans-MacBook-Pro.local>
2025-01-09 17:27:20 +0530

    feat: add hover animations on Join the AsyncAPI Conf in Paris! button

--- components/announcement.js
+++ components/announcement.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Announcement() {
   return (
     <Link href="/venue/Paris">
-    <div className='cursor-pointer border text-white min-w-[300px] rounded-lg p-1 text-center text-lg transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:bg-white hover:text-black'>Join the AsyncAPI Conf in Paris!</div>
+    <div className='cursor-pointer border text-white min-w-[300px] rounded-lg p-1 text-center text-lg transition-all duration-350 transform hover:scale-105 hover:shadow-lg hover:bg-white hover:text-black'>Join the AsyncAPI Conf in Paris!</div>
     </Link>
   )
 }
