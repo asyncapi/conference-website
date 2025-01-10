42feaabf17dd55e7351629a739a0b92707ab5619
Vedant Damedhar <damedharvedant1.com>
2025-01-10 20:16:18 +0530

    feat: Add hover animations to 'Join the AsyncAPI Conf in Paris' button

--- components/announcement.js
+++ components/announcement.js
@@ -4,7 +4,9 @@ import Link from 'next/link';
 export default function Announcement() {
   return (
     <Link href="/venue/Paris">
-    <div className='cursor-pointer border text-white min-w-[300px] rounded-lg p-1 text-center text-lg'>Join the AsyncAPI Conf in Paris!</div>
+      <div className="announcement">
+        <span>Join the AsyncAPI Conf in Paris!</span>
+      </div>
     </Link>
-  )
+  );
 }
