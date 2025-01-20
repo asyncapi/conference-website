512f34dc94dfafb9ce838b6355c97414898e274d
anmolgupta2015 <anmolgupta9872148833@gmail.com>
2025-01-20 08:41:20 +0530

    Added white Shadow around button

--- components/Scroll-button/ScrollToTop.js
+++ components/Scroll-button/ScrollToTop.js
@@ -29,8 +29,12 @@ const ScrollToTop = () => {
     isVisible && (
       <button
         onClick={scrollToTop}
-        className="fixed bottom-8 right-4 sm:right-8 flex items-center gap-2 px-3 sm:px-4 py-2 bg-gradient-to-r from-indigo-500 to-purple-600 text-white rounded-full shadow-lg hover:from-indigo-600 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 z-50"
-        style={{ fontSize: '0.875rem', lineHeight: '1.25rem' }}
+        className="fixed bottom-8 right-4 sm:right-8 flex items-center gap-2 px-3 sm:px-4 py-2 bg-gradient-to-r from-indigo-500 to-purple-600 text-white rounded-full shadow-lg hover:from-indigo-600 hover:to-purple-700 focus:outline-none focus:ring-4 focus:ring-offset-2 focus:ring-white focus:ring-opacity-80 active:ring-4 active:ring-white active:ring-opacity-100 z-50 transition-all duration-300 ease-in-out transform hover:scale-110"
+        style={{
+          fontSize: '0.875rem',
+          lineHeight: '1.25rem',
+          boxShadow: '0 8px 12px rgba(255, 255, 255, 0.5)', // Subtle white shadow for appeal
+        }}
         aria-label="Scroll to top"
       >
         <svg
