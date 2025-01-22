fb839e5e2f20ec572f7a08e1ed2f887222941242
Anmol yadav <anmolyadav03unnao@gmail.com>
2025-01-22 21:57:48 +0530

    added heading

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
-
-
 export default function Home() {
-	return (
-		<div className="flex sm:flex-col flex-row items-center justify-center w-full h-screen gap-6 ">
-			<img src="https://www.asyncapi.com/favicon.ico" className="scale-125  "></img>
-			<p className="font-bold text-white sm:text-4xl text-7xl shadow-lg  " style={{ textShadow: "#9D00FF 1px 0 8px" }}>
-				COMING SOON
-			</p>
-		</div>
-	)
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-screen gap-6">
+            
+			<div
+                className="text-8xl sm:text-5xl font-bold text-center"
+                style={{
+                    background: "linear-gradient(230deg, #e74694, #4b79cf, #4bc5cf, #4b79cf, #e74694)",
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent"
+                }}
+            >
+                AsyncAPI Conf 2025
+            </div>
+            <div className="flex sm:flex-col flex-row gap-6 items-center">
+                
+                <p
+                    className="font-bold text-white sm:text-4xl text-7xl shadow-lg"
+                    style={{ textShadow: "#9D00FF 1px 0 8px" }}
+                >
+                    COMING SOON
+                </p>
+            </div>
+        </div>
+    )
 }
+
