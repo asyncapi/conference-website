47762e857bc0b764a212296b5d3e036d367636b2
Rohan <r0hansingh@Rohans-MacBook-Pro.local>
2025-01-23 14:17:03 +0530

    style: add the requested changes

--- components/announcement.js
+++ components/announcement.js
@@ -4,7 +4,9 @@ import Link from 'next/link';
 export default function Announcement() {
   return (
     <Link href="/venue/Paris">
-    <div className='cursor-pointer border text-white min-w-[300px] rounded-lg p-1 text-center text-lg transition-all duration-350 transform hover:scale-105 hover:shadow-lg hover:backdrop-blur-xl'>Join the AsyncAPI Conf in Paris!</div>
+      <div className="cursor-pointer border text-white min-w-[300px] rounded-lg p-1 text-center text-lg transition-all duration-500 transform hover:scale-105 hover:shadow-lg hover:backdrop-blur-xl bg-transparent hover:bg-[linear-gradient(225deg,_#2DCCFD_9.35%,_#AD20E2_88.41%)]">
+        Join the AsyncAPI Conf in Paris!
+      </div>
     </Link>
   )
 }
