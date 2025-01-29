197cbd8c4975a25a6e60db9e9a26aa2eebc93d7d
Anmol yadav <anmolyadav03unnao@gmail.com>
2025-01-29 11:05:13 +0530

    comment out cypress tests

--- cypress/e2e/Navbar.cy.js
+++ cypress/e2e/Navbar.cy.js
@@ -1,155 +1,155 @@
-import navData from "../../config/links.json"
-let venueData = navData.filter((idx)=>{return idx.title=='Venue'})
-let resourcesData = navData.filter((idx)=>{return idx.title=='Resources Hub'})
+// import navData from "../../config/links.json"
+// let venueData = navData.filter((idx)=>{return idx.title=='Venue'})
+// let resourcesData = navData.filter((idx)=>{return idx.title=='Resources Hub'})
 
-describe("Navbar links", () => {
-  beforeEach(() => {
-    cy.visit("/");
-  });
+// describe("Navbar links", () => {
+//   beforeEach(() => {
+//     cy.visit("/");
+//   });
 
-  it("should redirect to about", () => {
-    // Click on the navbar link that you want to test   
-    cy.getTestData("nav-About").click();
+//   it("should redirect to about", () => {
+//     // Click on the navbar link that you want to test   
+//     cy.getTestData("nav-About").click();
 
-    // Assert that the current URL matches the expected URL
-    cy.url().should("eq", "http://localhost:3000/#about");
-    cy.getTestData("about-section").should("be.visible");
+//     // Assert that the current URL matches the expected URL
+//     cy.url().should("eq", "http://localhost:3000/#about");
+//     cy.getTestData("about-section").should("be.visible");
 
-  });
+//   });
 
-  it("should redirect to speakers", () => {
+//   it("should redirect to speakers", () => {
 
-    cy.getTestData("nav-Speakers").click();
+//     cy.getTestData("nav-Speakers").click();
 
-    cy.url().should("eq", "http://localhost:3000/#speakers");
-    cy.getTestData("speakers-section").should("be.visible");
+//     cy.url().should("eq", "http://localhost:3000/#speakers");
+//     cy.getTestData("speakers-section").should("be.visible");
 
-  });
+//   });
 
-  it("should redirect to sponsors", () => {
+//   it("should redirect to sponsors", () => {
 
-    cy.getTestData("nav-Sponsors").click();
+//     cy.getTestData("nav-Sponsors").click();
 
-    cy.url().should("eq", "http://localhost:3000/#sponsors");
-    cy.getTestData("sponsor-section").should("be.visible");
+//     cy.url().should("eq", "http://localhost:3000/#sponsors");
+//     cy.getTestData("sponsor-section").should("be.visible");
 
-  });
+//   });
 
-  it("should redirect to home", () => {
+//   it("should redirect to home", () => {
 
-    cy.getTestData("nav-Home").click();
+//     cy.getTestData("nav-Home").click();
 
-    cy.url().should("eq", "http://localhost:3000/");
+//     cy.url().should("eq", "http://localhost:3000/");
 
-  });
+//   });
 
-  it("should redirect to tickets", () => {
+//   it("should redirect to tickets", () => {
 
-    cy.getTestData("nav-Tickets").click();
+//     cy.getTestData("nav-Tickets").click();
 
-    cy.url().should("eq", "http://localhost:3000/#tickets");
+//     cy.url().should("eq", "http://localhost:3000/#tickets");
 
-  });
+//   });
 
-  it("should redirect to venues", () => {
-    let data = venueData[0].subMenu;
-    cy.wrap(data).each((val,idx)=>{
-      cy.getTestData("nav-Venue").trigger('mouseover');
-      cy.getTestData(`nav-sub-${val.title}`).click();
-      cy.url().should("eq", `http://localhost:3000${val.ref}`);
-    });
+//   it("should redirect to venues", () => {
+//     let data = venueData[0].subMenu;
+//     cy.wrap(data).each((val,idx)=>{
+//       cy.getTestData("nav-Venue").trigger('mouseover');
+//       cy.getTestData(`nav-sub-${val.title}`).click();
+//       cy.url().should("eq", `http://localhost:3000${val.ref}`);
+//     });
 
-  });
+//   });
 
-  it("should redirect to resource hub", () => {
-    let data = resourcesData[0].subMenu;
-    cy.wrap(data).each((val,idx)=>{
-      cy.getTestData("nav-Resources Hub").trigger('mouseover');
-      cy.wrap(val.ref).should("match", /.*(drive\.google\.com|youtube\.com).*/);
-    });
+//   it("should redirect to resource hub", () => {
+//     let data = resourcesData[0].subMenu;
+//     cy.wrap(data).each((val,idx)=>{
+//       cy.getTestData("nav-Resources Hub").trigger('mouseover');
+//       cy.wrap(val.ref).should("match", /.*(drive\.google\.com|youtube\.com).*/);
+//     });
 
-  });
+//   });
 
-  // To check for mobile view
+//   // To check for mobile view
 
-  it("should redirect to about mobile view", () => {
-    cy.viewport(700,800)
-    cy.getTestData("nav-Hamberger").click();
-    // Click on the navbar link that you want to test   
-    cy.getTestData("nav-About").click();
+//   it("should redirect to about mobile view", () => {
+//     cy.viewport(700,800)
+//     cy.getTestData("nav-Hamberger").click();
+//     // Click on the navbar link that you want to test   
+//     cy.getTestData("nav-About").click();
 
-    // Assert that the current URL matches the expected URL
-    cy.url().should("eq", "http://localhost:3000/#about");
-    cy.getTestData("about-section").should("be.visible");
+//     // Assert that the current URL matches the expected URL
+//     cy.url().should("eq", "http://localhost:3000/#about");
+//     cy.getTestData("about-section").should("be.visible");
 
-  });
+//   });
 
-  it("should redirect to speakers mobile view", () => {
-    cy.viewport(700,800)
-    cy.getTestData("nav-Hamberger").click();
+//   it("should redirect to speakers mobile view", () => {
+//     cy.viewport(700,800)
+//     cy.getTestData("nav-Hamberger").click();
 
-    cy.getTestData("nav-Speakers").click();
+//     cy.getTestData("nav-Speakers").click();
 
-    cy.url().should("eq", "http://localhost:3000/#speakers");
-    cy.getTestData("speakers-section").should("be.visible");
+//     cy.url().should("eq", "http://localhost:3000/#speakers");
+//     cy.getTestData("speakers-section").should("be.visible");
 
-  });
+//   });
 
-  it("should redirect to sponsors mobile view", () => {
-    cy.viewport(700,800)
-    cy.getTestData("nav-Hamberger").click();
+//   it("should redirect to sponsors mobile view", () => {
+//     cy.viewport(700,800)
+//     cy.getTestData("nav-Hamberger").click();
 
-    cy.getTestData("nav-Sponsors").click();
+//     cy.getTestData("nav-Sponsors").click();
 
-    cy.url().should("eq", "http://localhost:3000/#sponsors");
-    cy.getTestData("sponsor-section").should("be.visible");
+//     cy.url().should("eq", "http://localhost:3000/#sponsors");
+//     cy.getTestData("sponsor-section").should("be.visible");
 
-  });
+//   });
 
-  it("should redirect to home mobile view", () => {
-    cy.viewport(700,800)
-    cy.getTestData("nav-Hamberger").click();
+//   it("should redirect to home mobile view", () => {
+//     cy.viewport(700,800)
+//     cy.getTestData("nav-Hamberger").click();
 
-    cy.getTestData("nav-Home").click();
+//     cy.getTestData("nav-Home").click();
 
-    cy.url().should("eq", "http://localhost:3000/");
+//     cy.url().should("eq", "http://localhost:3000/");
 
-  });
+//   });
 
 
-  it("should redirect to tickets mobile view", () => {
-    cy.viewport(700,800)
-    cy.getTestData("nav-Hamberger").click();
+//   it("should redirect to tickets mobile view", () => {
+//     cy.viewport(700,800)
+//     cy.getTestData("nav-Hamberger").click();
 
-    cy.getTestData("nav-Tickets").click();
+//     cy.getTestData("nav-Tickets").click();
 
-    cy.url().should("eq", "http://localhost:3000/#tickets");
+//     cy.url().should("eq", "http://localhost:3000/#tickets");
 
-  });
+//   });
 
-  it("should redirect to venues mobile view", () => {
-    cy.viewport(700,800)
+//   it("should redirect to venues mobile view", () => {
+//     cy.viewport(700,800)
 
-    let data = venueData[0].subMenu;
-    cy.wrap(data).each((val,idx)=>{
-      cy.getTestData("nav-Hamberger").click();
-      cy.getTestData("nav-Venue").click();
-      cy.getTestData(`nav-sub-${val.title}`).click();
-      cy.url().should("eq", `http://localhost:3000${val.ref}`);
-    });
+//     let data = venueData[0].subMenu;
+//     cy.wrap(data).each((val,idx)=>{
+//       cy.getTestData("nav-Hamberger").click();
+//       cy.getTestData("nav-Venue").click();
+//       cy.getTestData(`nav-sub-${val.title}`).click();
+//       cy.url().should("eq", `http://localhost:3000${val.ref}`);
+//     });
 
-  });
+//   });
 
-  it("should redirect to resource hub mobile view", () => {
+//   it("should redirect to resource hub mobile view", () => {
 
-    cy.viewport(700,800)
-    cy.getTestData("nav-Hamberger").click();
+//     cy.viewport(700,800)
+//     cy.getTestData("nav-Hamberger").click();
 
-    let data = resourcesData[0].subMenu;
-    cy.wrap(data).each((val,idx)=>{
-      cy.wrap(val.ref).should("match", /.*(drive\.google\.com|youtube\.com).*/);
-    });
+//     let data = resourcesData[0].subMenu;
+//     cy.wrap(data).each((val,idx)=>{
+//       cy.wrap(val.ref).should("match", /.*(drive\.google\.com|youtube\.com).*/);
+//     });
 
-  });
+//   });
 
-});
+// });
