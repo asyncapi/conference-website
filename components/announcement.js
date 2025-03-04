c16593d82ba19e2aa57cbd451a8d5e7555d78e85
V Thulisile Sibanda <66913810+thulieblack@users.noreply.github.com>
2025-03-04 12:36:42 +0200

    feat: add singapore speakers (#572)
    Co-authored-by: thulieblack <sibanda.thulie@gmail.com>

--- components/announcement.js
+++ components/announcement.js
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function Announcement() {
   return (
-    <Link href="/venue/Singapore">
-    <div className='cursor-pointer border text-white min-w-[300px] rounded-lg p-1 text-center text-lg'>Call for Speakers Singapore!</div>
+    <Link href="/venue/Munich">
+    <div className='cursor-pointer border text-white min-w-[300px] rounded-lg p-1 text-center text-lg'>Call for Speakers Munich!</div>
     </Link>
   )
 }
