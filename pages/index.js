8ebd183d59329638cd40f90a424f1df9eae50ef8
TenzDelek <tibetdelek@gmail.com>
2025-03-05 12:27:49 +0530

    spacing fix

--- pages/index.js
+++ pages/index.js
@@ -95,7 +95,7 @@ export default function Home() {
                   </div>
                 ) : (
                   <div className="flex justify-center">
-                    <div className="w-[900px] lg:w-full flex justify-between">
+                    <div className="space-x-4 lg:w-full flex justify-between">
                       {speakers.map((speaker) => {
                         return (
                           <div
