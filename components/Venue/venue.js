7d6b85f341bcb4071e1c180bfd12654d921cbc4b
tanmay <kuamrchaurasiatanmay@gmail.com>
2025-03-11 17:33:21 +0530

    feat: Improve Navbar dropdown UI/UX (asyncapi#585)

--- components/Venue/venue.js
+++ components/Venue/venue.js
@@ -3,35 +3,35 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-export function getEventStatus(date){
+export function getEventStatus(date) {
 	const today = new Date();
-    const event = new Date(date);
+	const event = new Date(date);
 
-    if (event.toDateString() === today.toDateString()) {
-        return "Ongoing";
-    } else if (event > today) {
-        return "Upcoming";
-    } else {
-        return "Ended";
-    }
+	if (event.toDateString() === today.toDateString()) {
+		return "Ongoing";
+	} else if (event > today) {
+		return "Upcoming";
+	} else {
+		return "Ended";
+	}
 }
 
-export function isEventEnded(date){
+export function isEventEnded(date) {
 	return getEventStatus(date) === "Ended";
 }
 
 function Venue({ city }) {
 
 	const eventEnded = isEventEnded(city.date);
-	const textColor = eventEnded ? "text-white": "text-white";
+	const textColor = eventEnded ? "text-white" : "text-white";
 	const eventStatus = getEventStatus(city.date);
 
 	return (
 		<Link href={`/venue/${city.name}`}>
-			<div style={{background: `linear-gradient(to bottom, rgba(0,0,0,.3), rgba(0,0,0, .3)), url(${city.img})`, backgroundSize: "cover"}}
-className='relative w-[300px] h-[400px] sm:w-[250px] sm:h-[350px] card-bg rounded-md bg-[image:var(--image-url)] flex items-center justify-center p-4 cursor-pointer m-3 hover:scale-105 hover:shadow-xl transition-transform duration-300 ease-in-out'>				<div className='flex justify-between flex-col w-full h-full'>
+			<div style={{ background: `linear-gradient(to bottom, rgba(0,0,0,.3), rgba(0,0,0, .3)), url(${city.img})`, backgroundSize: "cover" }}
+				className='relative w-[300px] h-[400px] sm:w-[250px] sm:h-[350px] card-bg rounded-md bg-[image:var(--image-url)] flex items-center justify-center p-4 cursor-pointer m-3 hover:scale-105 hover:shadow-xl transition-transform duration-300 ease-in-out'>				<div className='flex justify-between flex-col w-full h-full'>
 					<div className='flex items-center'>
-						{city.cfp? <div className={`border ${textColor} text-md rounded-lg p-1 text-center mt-2`}>cfp is open</div> :null}
+						{city.cfp ? <div className={`border ${textColor} text-md rounded-lg p-1 text-center mt-2`}>cfp is open</div> : null}
 						<button onClick={(e) => {
 							e.preventDefault()
 							window.open(city.map, '_blank', 'noopener')
@@ -41,7 +41,7 @@ className='relative w-[300px] h-[400px] sm:w-[250px] sm:h-[350px] card-bg rounde
 					<div></div>
 					<div className={textColor}>
 						<div>
-							{city.name=='Online'?<span className='text-lg font-bold'>{city.name} {city.country}</span>:<span className='text-lg font-bold'>{city.country}, {city.name}</span>}
+							{city.name == 'Online' ? <span className='text-lg font-bold'>{city.name} {city.country}</span> : <span className='text-lg font-bold'>{city.country}, {city.name}</span>}
 						</div>
 						<div className='flex align-end flex-row justify-between pt-2'>
 							<div className='inline-block border border-gray-400 rounded-lg py-1 px-2 text-center sm:text-sm text-base'>{city.date}</div>
