f3b728b1bc0a27972d94b671a24ba47d8279a1b2
Robinaditya1045 <mahamuniaditya07@gmail.com>
2025-03-26 03:02:43 +0530

    chore: added svg to /illustratin dir

--- components/Buttons/scrollToTop.js
+++ components/Buttons/scrollToTop.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ChevronUp } from 'lucide-react';
+import ScrollToTopArrow from '../illustration/scrollToTopArrow';
 
 const ScrollToTop = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -32,7 +32,7 @@ const ScrollToTop = () => {
         onClick={scrollToTop} 
         className="gradient-bg text-white m-5 p-3 rounded-full shadow-lg transition-transform duration-300 transform hover:scale-110 flex justify-center items-center md:p-3 md:m-2 sm:p-2 sm:m-1"
       >
-        <ChevronUp size={24} className="md:w-8 md:h-8 sm:w-6 sm:h-6" />
+        <ScrollToTopArrow className="md:w-8 md:h-8 sm:w-6 sm:h-6" color="white" />
       </button>
     </div>
   );
---
+++ components/illustration/scrollToTopArrow.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function ScrollToTopArrow({ className, color }) {
+  return (
+    <svg
+      width="20"
+      height="20"
+      className={className}
+      viewBox="0 0 330 330"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75 
+           c-3.979,0-7.794,1.581-10.607,4.394l-149.996,150 
+           c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0 
+           l139.39-139.393l139.397,139.393 
+           C307.322,253.536,311.161,255,315,255 
+           c3.839,0,7.678-1.464,10.607-4.394 
+           C331.464,244.748,331.464,235.251,325.606,229.393z"
+        fill={color}
+      />
+    </svg>
+  );
+}
+
+export default ScrollToTopArrow;
