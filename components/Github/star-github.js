73e0712b937acf9d36af2ef47daa957775ffd65b
Jaishree2310 <jaishrees23102001@gmail.com>
2025-04-01 19:15:50 +0530

    feat: removed react-icon

--- components/Github/star-github.js
+++ components/Github/star-github.js
@@ -1,9 +1,13 @@
 'use client';
 
 import React from 'react';
-import { FaGithub } from 'react-icons/fa';
 
-function GithubButton({ text = 'Star on GitHub', href = 'https://github.com/asyncapi/conference-website', className = '', inNav = false }) {
+function GithubButton({
+  text = 'Star on GitHub',
+  href = 'https://github.com/asyncapi/conference-website',
+  className = '',
+  inNav = false,
+}) {
   const handleClick = () => {
     window.open(href, '_blank', 'noopener,noreferrer');
   };
@@ -12,10 +16,14 @@ function GithubButton({ text = 'Star on GitHub', href = 'https://github.com/asyn
     <button
       onClick={handleClick}
       className={`flex items-center gap-1 bg-white/20 hover:bg-white/40 
-      text-white px- py-2 rounded-md shadow-md font-medium whitespace-nowrap
+      text-white px-1 py-2 rounded-md shadow-md font-medium whitespace-nowrap
       transition-all duration-300 ${inNav ? 'h-10' : 'h-12'} ${className}`}
     >
-      <FaGithub className="text-lg" />
+      <img 
+        src="/img/github-logo.png" 
+        alt="GitHub" 
+        className="w-6 h-6" 
+      />
       <span className="inline-block">{text}</span>
     </button>
   );
