afcf2cc1d80e03fe0a48e56960e2ab07988c19c2
Ram Shukla <codewithrex@gmail.com>
2025-04-09 03:12:19 +0530

    Enhance social section to improve user engagement
    Fixes #659

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -1,32 +1,16 @@
 import React from "react";
-import Image from "next/image";
 import ILink from "../illustration/link";
+import Socials from "./socials";
 
 function Footer() {
-  const socials = [
-    {
-      name: "Github",
-      href: "https://github.com/asyncapi",
-      imgUrl: "/img/Github.png",
-    },
-    {
-      name: "Linkedin",
-      href: "https://www.linkedin.com/company/asyncapi/",
-      imgUrl: "/img/Linkedln.png",
-    },
-    {
-      name: "Twitter(X)",
-      href: "https://x.com/asyncapispec",
-      imgUrl: "/img/twitter_new.png",
-    },
-  ];
   return (
     <div className="container" data-test="footer">
       <div
-        className="w-full flex justify-between items-center p-4 sm:flex-col sm:gap-3"
+        className="w-full flex flex-col md:flex-row justify-between items-center p-4 gap-6"
         data-test="footer-asyncAPI-logo"
       >
-        <div className="mt-2 text-[14px] text-gray-100 sm:hidden">
+        {/* Code of Conduct Section */}
+        <div className="text-[20px] text-gray-100 flex items-center">
           <a
             href="https://github.com/asyncapi/community/blob/master/CODE_OF_CONDUCT.md"
             target="_blank"
@@ -34,34 +18,20 @@ function Footer() {
             className="hover:underline text-white duration-200 ease-in-out flex items-center"
             data-test="code-of-conduct"
           >
-           <span> Code of Conduct </span><span><ILink className='w-4 ml-2' fill='white' /></span>
+            <span>Code of Conduct</span>
+            <ILink className="w-4 ml-2" fill="white" />
           </a>
         </div>
-        <div></div>
-        <div className="flex items-center justify-between sm:flex-col sm:items-center">
-         <div className="text-white">Made with ❤️ by AsyncAPI contributors. By the community for the community!</div>
-         <div className="w-[0.9px] h-4 bg-white ml-4 sm:hidden" />
-            <div className="ml-4 flex justify-between items-center gap-2 sm:mt-4">
-              {socials.map((social, index) => {
-                return (
-                  <a
-                    key={index}
-                    href={social.href}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="rounded-lg flex items-center justify-center hover:border-[#AD20E2] duration-150 ease-in-out"
-                    data-test={`footer-${social.name}`}
-                  >
-                    <Image
-                      src={social.imgUrl}
-                      alt={social.name}
-                      height={23}
-                      width={23}
-                    />
-                  </a>
-                );
-              })}
-            </div>
+
+        {/* "Made with ❤️" and Socials Section */}
+        <div className="flex flex-col md:flex-row items-center gap-4 text-center md:text-left">
+          <span className="text-white text-[20px]">
+            Made with ❤️ by AsyncAPI contributors. By the community for the community!
+          </span>
+          <div className="w-full md:w-[1px] h-[1px] md:h-6 bg-white md:block hidden" />
+          <div className="flex items-center gap-2">
+            <Socials />
+          </div>
         </div>
       </div>
     </div>
