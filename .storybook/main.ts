0159dbc5d8f5019e1c9cabc141c2d344a3f3c38c
TenzDelek <tibetdelek@gmail.com>
2025-06-02 21:15:59 +0530

    feat: integrate Storybook into the project

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: [
+    "../components/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+
+  ],
+  docs: {
+    defaultName: 'Documentation',
+  },
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"],
+};
+export default config;
