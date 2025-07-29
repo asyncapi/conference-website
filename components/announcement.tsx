e3fcb1d921f690f0a0b94a803d21cc48adfc1894
V Thulisile Sibanda <66913810+thulieblack@users.noreply.github.com>
2025-07-29 13:01:22 +0200

    Update announcement.tsx

--- components/announcement.tsx
+++ components/announcement.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 function Announcement(): JSX.Element {
   return (
-    <Link href="/venue/London">
+    <Link href="/venue/Paris">
       <div className="cursor-pointer border text-white min-w-[300px] rounded-lg p-1 text-center text-lg">
         Call for Speakers Paris!
       </div>
