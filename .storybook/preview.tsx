7f011c9622f860ada190e59f49e6b258fcae2471
TenzDelek <tibetdelek@gmail.com>
2025-09-11 00:14:48 +0530

    upgrade to storybook 9

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/globals.css";
-import type { Preview } from "@storybook/react";
-import { themes } from '@storybook/theming';
+import type { Preview } from "@storybook/nextjs";
+import { themes } from 'storybook/theming';
 import {
   Title,
   Subtitle,
@@ -9,7 +9,7 @@ import {
   Primary,
   Controls,
   Stories,
-} from '@storybook/blocks';
+} from '@storybook/addon-docs/blocks';
 
 const preview: Preview = {
   tags: ['autodocs'],
