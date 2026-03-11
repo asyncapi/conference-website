efd29f0b3511083b40fcc826a60b5dda47de0a0a
DarshanCode2005 <darshanthakaregsoc2023@gmail.com>
2026-03-11 10:07:47 +0530

    fix(hero): make CTA elements responsive for small devices

--- components/announcement.tsx
+++ components/announcement.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 function Announcement(): JSX.Element {
   return (
     <Link href="/venue/Amsterdam">
-      <div className="cursor-pointer border text-white min-w-[300px] rounded-lg p-1 text-center text-lg transition duration-300 ease-in-out  hover:bg-white hover:text-black hover:scale-105 hover:shadow-lg">
+      <div className="cursor-pointer border text-white w-full max-w-[300px] rounded-lg p-1 text-center text-lg transition duration-300 ease-in-out hover:bg-white hover:text-black hover:scale-105 hover:shadow-lg">
         Submit a Proposal
       </div>
     </Link>
