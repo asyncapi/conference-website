3b5e96e10ca5778c0983e1be1ed5f040aac8ea98
TenzDelek <tibetdelek@gmail.com>
2026-03-30 21:43:33 +0530

    fix cypress

--- app/venue/[id]/page.tsx
+++ app/venue/[id]/page.tsx
@@ -171,7 +171,6 @@ export default async function VenuePage({
                   height="100%"
                   src={city.playlist}
                   title="YouTube video player"
-                  frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   referrerPolicy="strict-origin-when-cross-origin"
                   allowFullScreen
