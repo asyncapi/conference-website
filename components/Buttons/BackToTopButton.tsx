71655e9b3f05c6af69a59d01af35ecbf7f276baf
TenzDelek <tibetdelek@gmail.com>
2026-08-31 22:14:05 +0530

    fix on button

--- components/Buttons/BackToTopButton.tsx
+++ components/Buttons/BackToTopButton.tsx
@@ -42,7 +42,7 @@ const BackToTopButton: React.FC = () => {
   return isVisible ? (
     <button
       onClick={scrollToTop}
-      className="group fixed bottom-8 right-8 w-12 h-12 rounded-full border-none font-semibold flex items-center justify-center cursor-pointer overflow-hidden z-50 outline-none transition-all duration-300 ease-in-out transform hover:scale-95 active:scale-90 bg-[rgb(20,20,20)] shadow-[0px_0px_0px_4px_rgba(180,160,255,0.253)] hover:w-[140px] hover:rounded-[50px] hover:bg-[rgb(181,160,255)]"
+      className="group fixed bottom-8 right-8 w-12 h-12 rounded-full border-none font-semibold flex items-center justify-center cursor-pointer overflow-hidden z-50 outline-none transition-all duration-300 ease-in-out transform hover:scale-95 active:scale-90 bg-[rgb(20,20,20)] shadow-[0px_0px_0px_4px_rgba(180,160,255,0.253)] hover:w-36 hover:rounded-full hover:bg-[rgb(181,160,255)]"
       aria-label="Back to top"
     >
       <Arrows
@@ -50,7 +50,7 @@ const BackToTopButton: React.FC = () => {
         direction="up"
         fill="white"
       />
-      <span className="absolute text-white text-xs opacity-0 whitespace-nowrap transition-all duration-300 ease-in-out group-hover:text-[13px] group-hover:opacity-100">
+      <span className="absolute text-white text-xs opacity-0 whitespace-nowrap transition-all duration-300 ease-in-out group-hover:text-xs group-hover:opacity-100">
         Back to Top
       </span>
     </button>
